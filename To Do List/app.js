ce7daad9ba3d874820ef985d01c040ae7e2c86a6
ankit007 <ankeetpoudel6@gmail.com>
2023-04-11 11:06:07 +0545

    CSS Update

--- To Do List/app.js
+++ To Do List/app.js
@@ -1,10 +1,47 @@
-const todoList = [];
+const todoList = [{
+    name: 'make dinner', 
+    dueDate: '2020-12-13'
+}, {
+    name: 'wash dishes',
+    dueDate: '2023-12-13'
+}];
+
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHTML = ``;
+
+    for(let i=0; i<todoList.length; i++){
+        const todoObject = todoList[i];
+        const {name, dueDate} = todoObject;
+
+        const html = `
+        
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button" onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+        ">Delete</button>
+        `;
+        todoListHTML += html;
+    }
+    
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
 function addTodo(){
-    const inputElement = document.querySelector(`.name-input`);
+    const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
     
-    todoList.push(name);
-    console.log(todoList);
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        name, 
+        dueDate
+    });
 
     inputElement.value = '';
+    renderTodoList();
 }
